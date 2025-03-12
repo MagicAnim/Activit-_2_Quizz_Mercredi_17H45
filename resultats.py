98813acb0e8688fe8d5d9f4b5fc5c653f09ae35b
Abensett <bensettiaimen@mailo.com>
2025-03-12 18:34:13 +0100

    First Commit 18h48

---
+++ resultats.py
@@ -0,0 +1,6 @@
+resultats =  {
+        "Pikachu" : "Tu es enthousiame, plein d'entrain",
+        "Mew": "Tu es le calme incarnée",
+        "Salamèche": "Tu vis avec Passion",
+        "Carapuce": " Tu agis constamment de manière réfléchie"
+}
